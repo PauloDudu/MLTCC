a675cb67b33cc60bb11283e64d6405f6a2a2c28b
Paulo <dudu.sqa@alterdata.com.br>
2025-10-27 14:23:18 -0300

    Update Railway

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,7 +15,7 @@ def create_app() -> FastAPI:
     # CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://localhost:8080"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
